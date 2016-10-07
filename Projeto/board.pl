26c5b07e2a92ba7f19ea7cf99299911927dbe18d
Pedro Costa <pedrodc23@gmail.com>
2016-10-07 18:59:00 +0100

    Board is being drawn

---
+++ Projeto/board.pl
@@ -0,0 +1,27 @@
+board([
+    [cell,space,cell,space,cell,space,cell,space,cell,space,cell,space,cell,space,cell,space,cell,space,cell,space,cell,space,cell,space,cell,space,cell],
+    [null,cell,space,cell,space,cell,space,cell,space,cell,space,cell,space,cell,space,cell,space,cell,space,cell,space,cell,space,cell,space,cell,null],
+    [null,null,cell,space,cell,space,cell,space,cell,space,cell,space,cell,space,cell,space,cell,space,cell,space,cell,space,cell,space,cell,null,null],
+    [null,null,null,cell,space,cell,space,cell,space,cell,space,cell,space,cell,space,cell,space,cell,space,cell,space,cell,space,cell,null,null,null],
+    [null,null,null,null,cell,space,cell,space,cell,space,cell,space,cell,space,cell,space,cell,space,cell,space,cell,space,cell,null,null,null,null],
+    [null,null,null,null,null,cell,space,cell,space,cell,space,cell,space,cell,space,cell,space,cell,space,cell,space,cell,null,null,null,null,null],
+    [null,null,null,null,null,null,cell,space,cell,space,cell,space,cell,space,cell,space,cell,space,cell,space,cell,null,null,null,null,null,null]
+]).
+
+display_board([L1|LS]):-
+  display_line(L1),nl,display_board(LS).
+
+display_board([]):-nl.
+
+display_line([E1|ES]):-
+  traduz(E1,V),
+  write(V),
+  display_line(ES).
+
+display_line([]):-write('|').
+
+% TODO: when writing to the console, the space space is not being written
+traduz(cell  ,'[ ]').
+traduz(null  ,' - ').
+traudz(space ,'   ').
+traduz(E1    ,' | ').
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # PLOG-FEUP
+
+In this course we'll build the logical parte of the board game Pentalath, which is a 5-in-a-row game played on an unusual
+trapezoidal board, using Go capture rules.
+Game's rules: http://www.nestorgames.com/rulebooks/PENTALATH_EN.pdf
+Game's webpage: http://www.nestorgames.com/#pentalath_detail
