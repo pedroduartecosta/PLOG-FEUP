2a5bf1f6d4b563cc963e5d9d375df913204412d0
Pedro Costa <pedrodc23@gmail.com>
2016-10-09 18:11:45 +0100

    new hexagonal board

--- Projeto/board.pl
+++ Projeto/board.pl
@@ -1,11 +1,27 @@
 board([
-    [preto,space,cell,space,cell,space,cell,space,cell,space,cell,space,cell,space,cell,space,cell,space,cell,space,cell,space,cell,space,cell],
-    [null,branco,space,cell,space,cell,space,cell,space,cell,space,cell,space,cell,space,cell,space,cell,space,cell,space,cell,space,cell,null],
-    [null,null,cell,space,cell,space,cell,space,cell,space,cell,space,cell,space,cell,space,cell,space,cell,space,cell,space,cell,null,null],
-    [null,null,null,cell,space,cell,space,cell,space,cell,space,cell,space,cell,space,cell,space,cell,space,cell,space,cell,null,null,null],
-    [null,null,null,null,cell,space,cell,space,cell,space,cell,space,cell,space,cell,space,cell,space,cell,space,cell,null,null,null,null],
-    [null,null,null,null,null,cell,space,cell,space,cell,space,cell,space,cell,space,cell,space,cell,space,cell,null,null,null,null,null],
-    [null,null,null,null,null,null,cell,space,cell,space,cell,space,cell,space,cell,space,cell,space,cell,null,null,null,null,null,null]
+        [null,null,null,top,space,top,space,top,space,top,space,top,space,top,space,top,space,top,space,top,space,top,space,top,space,top,space,top,null,null,null,null],
+        [null,null,right,space,bottom,space,bottom,space,bottom,space,bottom,space,bottom,space,bottom,space,bottom,space,bottom,space,bottom,space,bottom,space,bottom,space,bottom,space,left,null,null,null],
+        [null,null,separator,cell,separator,cell,separator,cell,separator,cell,separator,cell,separator,cell,separator,cell,separator,cell,separator,cell,separator,cell,separator,cell,separator,cell,separator,cell,separator,null,null,null],
+        [null,null,left,space,top,space,top,space,top,space,top,space,top,space,top,space,top,space,top,space,top,space,top,space,top,space,top,space,right,null,null,null],
+        [null,null,null,bottom,space,bottom,space,bottom,space,bottom,space,bottom,space,bottom,space,bottom,space,bottom,space,bottom,space,bottom,space,bottom,space,bottom,space,bottom,null,null,null,null],
+        [null,null,null,separator,cell,separator,cell,separator,cell,separator,cell,separator,cell,separator,cell,separator,cell,separator,cell,separator,cell,separator,cell,separator,cell,separator,cell,separator,null,null,null,null],
+        [null,null,null,left,space,top,space,top,space,top,space,top,space,top,space,top,space,top,space,top,space,top,space,top,space,top,space,right,null,null,null,null],
+        [null,null,null,null,bottom,space,bottom,space,bottom,space,bottom,space,bottom,space,bottom,space,bottom,space,bottom,space,bottom,space,bottom,space,bottom,space,bottom,null,null,null,null],
+        [null,null,null,null,separator,cell,separator,cell,separator,cell,separator,cell,separator,cell,separator,cell,separator,cell,separator,cell,separator,cell,separator,cell,separator,cell,separator,null,null,null,null],
+        [null,null,null,null,left,space,top,space,top,space,top,space,top,space,top,space,top,space,top,space,top,space,top,space,top,space,right,null,null,null,null],
+        [null,null,null,null,null,bottom,space,bottom,space,bottom,space,bottom,space,bottom,space,bottom,space,bottom,space,bottom,space,bottom,space,bottom,space,bottom,null,null,null,null,null],
+        [null,null,null,null,null,separator,cell,separator,cell,separator,cell,separator,cell,separator,cell,separator,cell,separator,cell,separator,cell,separator,cell,separator,cell,separator,null,null,null,null,null],
+        [null,null,null,null,null,left,space,top,space,top,space,top,space,top,space,top,space,top,space,top,space,top,space,top,space,right,null,null,null,null,null],
+        [null,null,null,null,null,null,bottom,space,bottom,space,bottom,space,bottom,space,bottom,space,bottom,space,bottom,space,bottom,space,bottom,space,bottom,null,null,null,null,null],
+        [null,null,null,null,null,null,separator,cell,separator,cell,separator,cell,separator,cell,separator,cell,separator,cell,separator,cell,separator,cell,separator,cell,separator,null,null,null,null,null],
+        [null,null,null,null,null,null,left,space,top,space,top,space,top,space,top,space,top,space,top,space,top,space,top,space,right,null,null,null,null,null],
+        [null,null,null,null,null,null,null,bottom,space,bottom,space,bottom,space,bottom,space,bottom,space,bottom,space,bottom,space,bottom,space,bottom,null,null,null,null,null,null],
+        [null,null,null,null,null,null,null,separator,cell,separator,cell,separator,cell,separator,cell,separator,cell,separator,cell,separator,cell,separator,cell,separator,null,null,null,null,null,null],
+        [null,null,null,null,null,null,null,left,space,top,space,top,space,top,space,top,space,top,space,top,space,top,space,right,null,null,null,null,null,null],
+        [null,null,null,null,null,null,null,nullbottom,space,bottom,space,bottom,space,bottom,space,bottom,space,bottom,space,bottom,space,bottom,null,null,null,null,null,null],
+        [null,null,null,null,null,null,null,null,separator,cell,separator,cell,separator,cell,separator,cell,separator,cell,separator,cell,separator,cell,separator,null,null,null,null,null,null],
+        [null,null,null,null,null,null,null,null,left,space,top,space,top,space,top,space,top,space,top,space,top,space,right,null,null,null,null,null,null],
+        [null,null,null,null,null,null,null,null,null,bottom,space,bottom,space,bottom,space,bottom,space,bottom,space,bottom,space,bottom,null,null,null,null,null,null,null]
 ]).
 
 display_board([L1|LS]):-
@@ -18,14 +34,15 @@ display_line([E1|ES]):-
   write(V),
   display_line(ES).
 
-display_line([]):-write('|').
+display_line([]):-write('').
 
 % TODO: when writing to the console, the space space is not being written
-traduz(cell  ,'[ ]').
-traduz(null  ,' - ').
-traduz(space ,'   ').
-traduz(preto ,' P ').
-traduz(branco,' B ').
-traduz(E1    ,'   ').
-
-b_setval(pretas,40)
+traduz(cell     ,'   ').
+traduz(null     ,'   ').
+traduz(space    ,'   ').
+traduz(bottom   ,' V ').
+traduz(top      ,' ^ ').
+traduz(left     ,' ` ').
+traduz(right    ,' / ').
+traduz(separator,' | ').
+traduz(E1       ,'').
