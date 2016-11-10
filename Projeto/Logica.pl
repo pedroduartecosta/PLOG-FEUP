5f8c1a51d4fd7b2649e882786c0e8fd6be77f507
Pedro Costa <pedrodc23@gmail.com>
2016-11-10 10:12:19 +0000

    refactored

--- Projeto/Logica.pl
+++ Projeto/Logica.pl
@@ -2,17 +2,17 @@
 
 
 
-% replace: altera o contuedo do tabuleiro nas coordenadas X,Y
-replace([L|Ls],0,X,Z,[R|Ls]):-replace_line(L,L,X,Z,R).
+% replace: altera se possivel o contuedo do tabuleiro nas coordenadas X,Y
+replace([L|Ls],0,X,Z,[R|Ls]):-replace_line(L,X,Z,R).
 replace([L|Ls],Y,X,Z,[L|Rs]):-Y > 0, Y1 is Y-1, replace(Ls,Y1,X,Z,Rs).
 
-replace_line([_|Cs],b,0,Z,[_|Cs]):-
+replace_line([b|Cs],0,Z,[_|Cs]):-
   write('FALSE MOVE').
-replace_line([_|Cs],p,0,Z,[_|Cs]):-
+replace_line([p|Cs],0,Z,[_|Cs]):-
   write('FALSE MOVE').
 
-replace_line([_|Cs],A,0,Z,[Z|Cs]).
-replace_line([C|Cs],A,X,Z,[C|Rs]):-X > 0, X1 is X-1, replace_line(Cs,C,X1,Z,Rs).
+replace_line([_|Cs],0,Z,[Z|Cs]).
+replace_line([C|Cs],X,Z,[C|Rs]):-X > 0, X1 is X-1, replace_line(Cs,X1,Z,Rs).
 
 %
 return_value([L|Ls], X, 0, R]):- return_value_line(L, X, R).
@@ -37,4 +37,4 @@ verify_horizontal(T,PLAYER,X, Y, MAX_X, COUNT):-
 verify_horizontal(T,PLAYER,X, Y, MAX_X, MAX_Y, COUNT):-
   COUNT >0,
   return_value(T, X, Y, R),
-  & to be completed
+  %to be completed
