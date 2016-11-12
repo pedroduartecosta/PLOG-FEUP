cd5fe6d4ee10baf7058432e58067c73b2e1a1fa8
Pedro Costa <pedrodc23@gmail.com>
2016-11-12 16:34:58 +0000

    Corrected false move

--- Projeto/Utilities.pl
+++ Projeto/Utilities.pl
@@ -12,31 +12,39 @@ newLine(Line, Limit) :-
 
 newLine(_,_).
 
-getCoordinates(X,Y,MAX_X,MAX_Y,Z,1):-
+getCoordinates(T,X,Y,MAX_X,MAX_Y,Z,1):-
 	write('Y coordinate to place disk'),nl,
 	read(Y2),Y1 is Y2-1,Y1<(MAX_Y),
 	write('X coordinate to place disk'),nl,
 	read(X2),X1 is X2-1,X1<(MAX_X),
+	return_value(T,X1,Y1,R),
+	R == null,
 	Y is Y1,
 	X is X1.
 
 
-getCoordinates(X,Y,MAX_X,MAX_Y, b, 2):-
+getCoordinates(T,X,Y,MAX_X,MAX_Y, b, 2):-
 	random(0, MAX_Y, Y2),Y1 is Y2,Y1<(MAX_Y),
 	random(0, MAX_X, X2),X1 is X2,X1<(MAX_X-Y1),
+	return_value(T,X1,Y1,R),
+	R == null,
 	Y is Y1,
 	X is X1.
 
-getCoordinates(X,Y,MAX_X,MAX_Y,p, 2):-
+getCoordinates(T,X,Y,MAX_X,MAX_Y,p, 2):-
 	write('Y coordinate to place disk'),nl,
 	read(Y2),Y1 is Y2-1,Y1<(MAX_Y),
 	write('X coordinate to place disk'),nl,
 	read(X2),X1 is X2-1,X1<(MAX_X),
+	return_value(T,X1,Y1,R),
+	R == null,
 	Y is Y1,
 	X is X1.
 
-getCoordinates(X,Y,MAX_X,MAX_Y,Z, 3):-
+getCoordinates(T,X,Y,MAX_X,MAX_Y,Z, 3):-
 	random(0, MAX_Y, Y2),Y1 is Y2,Y1<(MAX_Y),
 	random(0, MAX_X, X2),X1 is X2,X1<(MAX_X-Y1),
+	return_value(T,X1,Y1,R),
+	R == null,
 	Y is Y1,
 	X is X1.
